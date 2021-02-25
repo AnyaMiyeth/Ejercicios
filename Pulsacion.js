c40b6ad07535e70340dd2a49b4f0d342aba37dbd
anyamiyeth <anyamiyeth@byasystems.com.co>
2021-02-25 16:20:23 -0500

    first commit

---
+++ Pulsacion.js
@@ -0,0 +1,12 @@
+function CalcularPulsacion() {
+    var edad = document.getElementById("edad").value;
+    var sexo = document.getElementById("sexo").value;
+    let pulsacion=0;
+    if(sexo==="F"){
+pulsacion=
+    }
+    else{
+
+    }
+
+}
